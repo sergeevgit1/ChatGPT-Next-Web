41bb4a6f3800d36f0fee46d84497c4eac5db1b37
Dmitriy Sergeev <83030369+sergeevgit1@users.noreply.github.com>
2024-03-01 20:35:41 +0300

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          Твой личный GPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          ChatGPT без VPN. GPT4 скоро!
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
